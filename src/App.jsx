516ffd0e2bb766746122c71d7be6248c8a993aee
namhnguy-home <namhnguy@gmail.com>
2025-03-18 15:01:14 -0500

    wip navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
-  )
-}
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      <Navbar />
+      {/* <Breadcrumb />
+      <Introduction />
+      <FeaturedPost />
+      <PostList /> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
