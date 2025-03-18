e811a546ab24c11af18de625a52c9990cb2720d8
namhnguy-home <namhnguy@gmail.com>
2025-03-18 15:39:25 -0500

    setup react router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
@@ -7,10 +8,10 @@ const Navbar = () => {
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
-      <div className="flex item-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex item-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="Byu Logo" w={32} h={32} />
         <span>byulog</span>
-      </div>
+      </Link>
       {/* mobile button */}
       <div className="md:hidden">
         <div
@@ -25,28 +26,28 @@ const Navbar = () => {
             open ? "-right-0" : "-right-[100%]"
           } font-medium text-lg gap-8`}
         >
-          <a href="">Home</a>
-          <a href="">Trending</a>
-          <a href="">Most Popular</a>
-          <a href="">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login ✌️
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* desktop menu */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="">Home</a>
-        <a href="">Trending</a>
-        <a href="">Most Popular</a>
-        <a href="">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login ✌️
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
